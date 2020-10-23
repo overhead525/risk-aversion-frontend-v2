5bfe4da09e192f82fef39218bc59a3a152e92ac6
overhead525 <ritchelcousar@outlook.com>
2020-10-22 20:25:55 -0400

    got main content scaffolding setup correctly using Carbon Design System Grids

--- src/components/screens/Dashboard/index.tsx
+++ src/components/screens/Dashboard/index.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-interface DashboardProps {};
+import "./styles.scss";
+
+interface DashboardProps {}
+
+const initialUser = {
+  name: "Malcolm Gladwell",
+};
 
 const Dashboard: React.FC<DashboardProps> = () => {
-    return <div>Dashboard</div>;
-}
+  const [user, setUser] = useState(initialUser);
+  return (
+    <div className="bx--grid">
+      <div className="bx--row mb__standard">
+        <div className="bx--col-md-5 scaffold" id="data">
+          <p>Dashboard</p>
+          <h1 className="username-hero">{user.name}</h1>
+        </div>
+        <div className="bx--col-md-3 scaffold" id="simulations-table">
+          2 of 2
+        </div>
+      </div>
+      <div className="bx--row">
+        <div className="bx--col scaffold">{"<Table Goes Here>"}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
