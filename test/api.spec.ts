aee40db8502c16a155533c9faf063203db167c1f
overhead525 <ritchelcousar@outlook.com>
2020-11-13 18:19:14 -0500

    changed port to avoid clashing with kubernetes

---
+++ test/api.spec.ts
@@ -0,0 +1,5 @@
+describe("send api requests inside of the risk-aversion network", () => {
+  test("send a get request to the core server", async () => {
+    fetch("http://core-server");
+  });
+});
