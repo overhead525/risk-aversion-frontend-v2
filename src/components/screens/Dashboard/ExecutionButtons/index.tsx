79d9f31cf53bd47a71298d7ea66b111032477667
overhead525 <ritchelcousar@outlook.com>
2020-11-18 14:48:27 -0500

    return to home button sends user...back home :)

--- src/components/screens/Dashboard/ExecutionButtons/index.tsx
+++ src/components/screens/Dashboard/ExecutionButtons/index.tsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { Button, ButtonProps } from "carbon-components-react";
 import { Add32, Home32 } from "@carbon/icons-react";
 // import { NewSimulationButton } from "../../../../stories/ra_Button/Button.stories";
 
-export const ExecutionButtons: React.FC = () => (
-  <div>
-    <Button kind="primary" renderIcon={Add32}>
-      New Simulation
-    </Button>
-    <Button kind="secondary" renderIcon={Home32}>
-      Return to Home
-    </Button>
-  </div>
-);
+export const ExecutionButtons: React.FC = () => {
+  const history = useHistory();
+  const goHome = () => history.push("/");
+
+  return (
+    <div>
+      <Button kind="primary" renderIcon={Add32}>
+        New Simulation
+      </Button>
+      <Button kind="secondary" renderIcon={Home32} onClick={goHome}>
+        Return to Home
+      </Button>
+    </div>
+  );
+};
