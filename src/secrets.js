6d84db01fbb18ca9a44c58d9e0b8346062bfef79
overhead525 <ritchelcousar@outlook.com>
2020-11-18 12:55:40 -0500

    updated secrets for proper url to sim server

--- src/secrets.js
+++ src/secrets.js
@@ -1,4 +1,4 @@
 export const AUTH_SERVER_URL = "auth-server/auth";
 export const SIM_SERVER_URL = 'core-server/graphql';
 export const IMAGE_SERVER_URL = 'image-server/image';
-export const PROFILE_SERVER_URL = 'profile-server/profile';
+export const PROFILE_SERVER_URL = 'http://localhost:4500/profile';
