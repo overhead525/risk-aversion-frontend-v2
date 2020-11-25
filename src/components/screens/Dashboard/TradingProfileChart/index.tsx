d06da8f3dba25fb5fc5f12c69fb316701f17cb77
overhead525 <ritchelcousar@outlook.com>
2020-11-25 11:21:26 -0500

    stopping point

--- src/components/screens/Dashboard/TradingProfileChart/index.tsx
+++ src/components/screens/Dashboard/TradingProfileChart/index.tsx
@@ -1,29 +1,14 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {
-  getUserProfile,
-  selectEntireProfile,
-} from "../../../../features/profile/profileSlice";
+import { selectEntireProfile } from "../../../../features/profile/profileSlice";
 import { convertRawProfile, composeRadarData, marketProfile } from "./helpers";
 // @ts-ignore
 import { RadarChart } from "@carbon/charts-react";
 
 export const TradingProfileChart: React.FC = () => {
-  const dispatch = useDispatch();
-
   const profile = useSelector(selectEntireProfile);
+
   console.log("Profile", profile);
-
-  const getUserProfileWrapper = async () => {
-    const accessToken =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiTG1hbGNvbG1SIiwiaWF0IjoxNjAzNjk2NzI1fQ.h5nXMrUYJyWaEP0wkVoxzx3HfM9deYcZHuAgTEL1AkQ";
-    const username = "LmalcolmR";
-    await dispatch(getUserProfile(accessToken, username));
-  };
-
-  useEffect(() => {
-    getUserProfileWrapper();
-  }, []);
 
   const data = [
     ...composeRadarData(convertRawProfile(profile.tradingHabits), "User"),
--- src/secrets.js
+++ src/secrets.js
@@ -1,4 +1,5 @@
 export const AUTH_SERVER_URL = "auth-server/auth";
-export const SIM_SERVER_URL = 'core-server/graphql';
+export const SIM_SERVER_URL = 'http://localhost:3000/graphql';
 export const IMAGE_SERVER_URL = 'image-server/image';
 export const PROFILE_SERVER_URL = 'http://localhost:4500/profile';
+export const PERMANENT_ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiTG1hbGNvbG1SIiwiaWF0IjoxNjAzNjk2NzI1fQ.h5nXMrUYJyWaEP0wkVoxzx3HfM9deYcZHuAgTEL1AkQ';
