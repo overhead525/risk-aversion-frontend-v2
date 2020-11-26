a627966da6fece58144d2abe5cc8401089366ff8
overhead525 <ritchelcousar@outlook.com>
2020-11-26 07:52:32 -0500

    added links to Execution Buttons

--- src/components/screens/Dashboard/ExecutionButtons/index.tsx
+++ src/components/screens/Dashboard/ExecutionButtons/index.tsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Button, ButtonProps } from "carbon-components-react";
 import { Add32, Home32 } from "@carbon/icons-react";
 // import { NewSimulationButton } from "../../../../stories/ra_Button/Button.stories";
 
 export const ExecutionButtons: React.FC = () => (
   <div>
-    <Button kind="primary" renderIcon={Add32}>
-      New Simulation
-    </Button>
-    <Button kind="secondary" renderIcon={Home32}>
-      Return to Home
-    </Button>
+    <Link to="/app/sim-setup" style={{ textDecoration: "none" }}>
+      <Button kind="primary" renderIcon={Add32}>
+        New Simulation
+      </Button>
+    </Link>
+    <Link to="/" style={{ textDecoration: "none" }}>
+      <Button kind="secondary" renderIcon={Home32}>
+        Return to Home
+      </Button>
+    </Link>
   </div>
 );
